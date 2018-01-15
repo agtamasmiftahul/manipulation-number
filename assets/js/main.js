22446eae5c9ae568526750491b4cfd4d774d0b6b
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-01-15 14:05:37 +0700

    Add js function to remainder

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,3 +77,18 @@ function showDivide() {
     Materialize.toast('Please, input a number', 1000);
   }
 }
+
+function showRemainder() {
+  number0 = level1a.value;
+  number1 = level1b.value;
+  if (isNaN(number0) !== true && isNaN(number1) !== true) {
+    result = Number(number0) % Number(number1);
+    Materialize.toast(`${number0} % ${number1} = ${result}`, 1000);
+  } else if (isNaN(number0) !== true && isNaN(number1) === true) {
+    Materialize.toast('Your first input is a number, but your second input is not a number. Please, input a number', 1000);
+  } else if (isNaN(number0) === true && isNaN(number1) !== true) {
+    Materialize.toast('Your second input is a number, but your first input is not a number. Please, input a number', 1000);
+  } else {
+    Materialize.toast('Please, input a number', 1000);
+  }
+}
