54910f9dfc35cb188f8ade9965c59120b7cb3716
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-01-15 13:59:08 +0700

    Add js function to divide and multiply

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,33 @@ function showSubstract() {
     Materialize.toast('Please, input a number', 1000);
   }
 }
+
+function showMultiply() {
+  number0 = level1a.value;
+  number1 = level1b.value;
+  if (isNaN(number0) !== true && isNaN(number1) !== true) {
+    result = Number(number0) * Number(number1);
+    Materialize.toast(`${number0} x ${number1} = ${result}`, 1000);
+  } else if (isNaN(number0) !== true && isNaN(number1) === true) {
+    Materialize.toast('Your first input is a number, but your second input is not a number. Please, input a number', 1000);
+  } else if (isNaN(number0) === true && isNaN(number1) !== true) {
+    Materialize.toast('Your second input is a number, but your first input is not a number. Please, input a number', 1000);
+  } else {
+    Materialize.toast('Please, input a number', 1000);
+  }
+}
+
+function showDivide() {
+  number0 = level1a.value;
+  number1 = level1b.value;
+  if (isNaN(number0) !== true && isNaN(number1) !== true) {
+    result = Number(number0) / Number(number1);
+    Materialize.toast(`${number0} / ${number1} = ${result}`, 1000);
+  } else if (isNaN(number0) !== true && isNaN(number1) === true) {
+    Materialize.toast('Your first input is a number, but your second input is not a number. Please, input a number', 1000);
+  } else if (isNaN(number0) === true && isNaN(number1) !== true) {
+    Materialize.toast('Your second input is a number, but your first input is not a number. Please, input a number', 1000);
+  } else {
+    Materialize.toast('Please, input a number', 1000);
+  }
+}
